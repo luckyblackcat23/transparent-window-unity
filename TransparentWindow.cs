584b74c79e77fbfac1f468c46f903b7058fc76b8
luckyblackcat23 <79121452+luckyblackcat23@users.noreply.github.com>
2021-02-23 14:56:21 +0000

    Update TransparentWindow.cs

--- TransparentWindow.cs
+++ TransparentWindow.cs
@@ -1,15 +1,3 @@
-﻿/* 
-    ------------------- Code Monkey -------------------
-
-    Thank you for downloading this package
-    I hope you find it useful in your projects
-    If you have any questions let me know
-    Cheers!
-
-               unitycodemonkey.com
-    --------------------------------------------------
- */
-
 using System;
 using System.Runtime.InteropServices;
 using System.Collections;
